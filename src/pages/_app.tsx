156b60f6b3546ef4b0959a81066feb6b1c1dec5d
jwphantom <44881349+jwphantom@users.noreply.github.com>
2023-08-18 04:26:15 +0100

    fix bug

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,4 @@
-//import "@/styles/vendor/aos/aos.css";
+import "@/styles/vendor/aos/aos.css";
 import "@/styles/vendor/bootstrap/css/bootstrap.min.css";
 
 import "@/styles/vendor/bootstrap-icons/bootstrap-icons.css";
