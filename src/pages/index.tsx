923d432849a2830c0a9ed9e76755e25424aff8ce
jwphantom <44881349+jwphantom@users.noreply.github.com>
2023-08-19 21:29:55 +0100

    fix bugs

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -63,13 +63,13 @@ export default function Home(props: any) {
                 {/* ======= Footer ======= */}
                 <Footer />
                 {/* End Footer */}
-                <div id="preloader"></div>
+                {/* <div id="preloader"></div>
                 <a
                     href="#"
                     className="back-to-top d-flex align-items-center justify-content-center"
                 >
                     <i className="bi bi-arrow-up-short"></i>
-                </a>
+                </a> */}
             </Layout>
         </>
     );
