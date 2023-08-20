0d06d73708f93c7796ea1058dcd629d8a9ee2a78
jwphantom <44881349+jwphantom@users.noreply.github.com>
2023-08-20 01:07:50 +0100

    fix bugs

--- public/js/main.js
+++ public/js/main.js
@@ -177,9 +177,9 @@
         });
     }
 
-    /**
-     * Porfolio isotope and filter
-     */
+    // /**
+    //  * Porfolio isotope and filter
+    //  */
     // window.addEventListener("load", () => {
     //     let portfolioContainer = select(".portfolio-container");
     //     if (portfolioContainer) {
@@ -211,71 +211,71 @@
     //     }
     // });
 
-    // /**
-    //  * Initiate portfolio lightbox
-    //  */
-    // const portfolioLightbox = GLightbox({
-    //     selector: ".portfolio-lightbox"
-    // });
+    /**
+     * Initiate portfolio lightbox
+     */
+    const portfolioLightbox = GLightbox({
+        selector: ".portfolio-lightbox"
+    });
 
-    // /**
-    //  * Initiate portfolio details lightbox
-    //  */
-    // const portfolioDetailsLightbox = GLightbox({
-    //     selector: ".portfolio-details-lightbox",
-    //     width: "90%",
-    //     height: "90vh"
-    // });
+    /**
+     * Initiate portfolio details lightbox
+     */
+    const portfolioDetailsLightbox = GLightbox({
+        selector: ".portfolio-details-lightbox",
+        width: "90%",
+        height: "90vh"
+    });
 
-    // /**
-    //  * Portfolio details slider
-    //  */
-    // new Swiper(".portfolio-details-slider", {
-    //     speed: 400,
-    //     loop: true,
-    //     autoplay: {
-    //         delay: 5000,
-    //         disableOnInteraction: false
-    //     },
-    //     pagination: {
-    //         el: ".swiper-pagination",
-    //         type: "bullets",
-    //         clickable: true
-    //     }
-    // });
+    /**
+     * Portfolio details slider
+     */
+    new Swiper(".portfolio-details-slider", {
+        speed: 400,
+        loop: true,
+        autoplay: {
+            delay: 5000,
+            disableOnInteraction: false
+        },
+        pagination: {
+            el: ".swiper-pagination",
+            type: "bullets",
+            clickable: true
+        }
+    });
 
-    // /**
-    //  * Testimonials slider
-    //  */
-    // new Swiper(".testimonials-slider", {
-    //     speed: 600,
-    //     loop: true,
-    //     autoplay: {
-    //         delay: 5000,
-    //         disableOnInteraction: false
-    //     },
-    //     slidesPerView: "auto",
-    //     pagination: {
-    //         el: ".swiper-pagination",
-    //         type: "bullets",
-    //         clickable: true
-    //     }
-    // });
+    /**
+     * Testimonials slider
+     */
+    new Swiper(".testimonials-slider", {
+        speed: 600,
+        loop: true,
+        autoplay: {
+            delay: 5000,
+            disableOnInteraction: false
+        },
+        slidesPerView: "auto",
+        pagination: {
+            el: ".swiper-pagination",
+            type: "bullets",
+            clickable: true
+        }
+    });
 
-    // /**
-    //  * Animation on scroll
-    //  */
-    // window.addEventListener("load", () => {
-    //     AOS.init({
-    //         duration: 1000,
-    //         easing: "ease-in-out",
-    //         once: true,
-    //         mirror: false
-    //     });
-    // });
+    /**
+     * Animation on scroll
+     */
+    window.addEventListener("load", () => {
+        AOS.init({
+            duration: 1000,
+            easing: "ease-in-out",
+            once: true,
+            mirror: false
+        });
+    });
 
-    // /**
-    //  * Initiate Pure Counter
-    //  */
-    // new PureCounter();
+    /**
+     * Initiate Pure Counter
+     */
+    new PureCounter();
 })();
